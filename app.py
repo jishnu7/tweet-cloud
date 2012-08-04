d308163e0f3be36ef6d78a72a8a1f14116b820f9
Jishnu <jishnu7@gmail.com>
2012-08-05 01:01:07 +0530

    Fixed several issues, Added options, Responsive layout

--- app.py
+++ app.py
@@ -4,38 +4,80 @@ from collections import defaultdict
 
 app = Flask(__name__)
 
-@app.route('/') 
+@app.route('/')
 def hello():
     return render_template('home.html')
- 
-@app.route('/get_tweets/<handle>') 
-def get_tweets(handle=""): 
+
+@app.route('/get_tweets/<handle>')
+def get_tweets(handle=""):
     count = request.args.get('count', 1000, type=int)
-    exclude_replies = request.args.get('exclude_replies', True, type=bool)
-    include_rts = request.args.get('include_rts', False, type=bool)
+    replies = request.args.get('replies', False, type=int)
+    include_rts = request.args.get('include_rts', False, type=int)
 
     base_url = "https://api.twitter.com/1/statuses/user_timeline.json"
     param = {
             'screen_name' : handle,
-            'exclude_replies' : exclude_replies,
+            'exclude_replies' : not replies,
             'include_rts' : include_rts,
             'count' : count
             }
     print param
+    
     url = base_url + '?' + urllib.urlencode(param)
     result = simplejson.load(urllib.urlopen(url))
 
+    type_value = request.args.get('type', 'word_count', type=str)
+    data = values(type_value, result)
+    json = simplejson.dumps(data)
+    resp = Response(json, status=200, mimetype='application/json')
+    return resp
+
+def values(option, data):
+    return {
+    'word_count': word_count,
+    'words': words,
+    'hashtags': hashtags
+    }.get(option, word_count)(data)
+
+def word_count(data):
     tweet_count = defaultdict(lambda:0)
-    if 'errors' in result:
+    if 'errors' in data:
         return None
     else:
-        for tweet in result:
+        for tweet in data:
             length = len(tweet['text'].split())
             tweet_count[length] += 1
     print dict(tweet_count)
-    json = simplejson.dumps(tweet_count)
-    resp = Response(json, status=200, mimetype='application/json')
-    return resp
+    return tweet_count
+
+def words(data):
+    tweets = defaultdict(lambda:0)
+    if 'errors' in data:
+        return None
+    else:
+        for tweet in data:
+            # TODO: Need to clean words. Improve later
+            words = tweet['text'].split()
+            for word in words:
+                tweets[word] += 1
+    # Remove words which has frequency 1
+    tweets = {key: value for key, value in tweets.items() if value != 1}
+    print dict(tweets)
+    return tweets
+
+def hashtags(data):
+    tweets = defaultdict(lambda:0)
+    if 'errors' in data:
+        return None
+    else:
+        for tweet in data:
+            # TODO: Need to clean hashtags. Improve later
+            words = tweet['text'].split()
+            for word in words:
+                if word.startswith("#"):
+                    tweets[word] += 1
+    print dict(tweets)
+    return tweets 
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
